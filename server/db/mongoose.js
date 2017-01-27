d7f1ed686488aef1f21d3cc4700377340b54c47c
alan <saxal28@gmail.com>
2017-01-26 22:20:28 -0600

    didnt have card on file so switching out for mblabs permanent serveR

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require("mongoose");
 //tells mongoose to use promises in default javascript
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.PORT ? "mongodb://saxal28:gatorade2@ds133279.mlab.com:33279/todo_app" : "mongodb://localhost:27017/TodoApp");
+mongoose.connect("mongodb://saxal28:gatorade2@ds133279.mlab.com:33279/todo_app");
 
 module.exports = {
   mongoose
